fe4af828210994cb24abe5de0e69d9b4b9571420
Michael Gernoth <michael@gernoth.net>
2016-03-29 17:27:46 +0400

    Exynos4RIL: Fix data after reboot
    In previous versions, setPreferredNetworkType was called directly after
    startup while the modem was powered down so the correct network type
    was available while the modem is initializing. This changed in M
    and the preferred network type is set much later which causes the
    modem to not allow datacalls until it is rebooted again.
    Rebooting the modem the first time setPreferredNetworkType is
    encountered fixes this.
    
    Change-Id: Idc046fde4bca6538ea48ef4ba4515870eb97806f

--- ril/telephony/java/com/android/internal/telephony/SamsungExynos4RIL.java
+++ ril/telephony/java/com/android/internal/telephony/SamsungExynos4RIL.java
@@ -31,6 +31,8 @@ import android.telephony.PhoneNumberUtils;
 
 public class SamsungExynos4RIL extends RIL implements CommandsInterface {
 
+    private boolean setPreferredNetworkTypeSeen = false;
+
     //SAMSUNG STATES
     static final int RIL_REQUEST_GET_CELL_BROADCAST_CONFIG = 10002;
 
@@ -424,6 +426,12 @@ public class SamsungExynos4RIL extends RIL implements CommandsInterface {
         int response = p.readInt();
 
         switch(response) {
+            case RIL_UNSOL_RIL_CONNECTED:
+                if (!setPreferredNetworkTypeSeen) {
+                    Rlog.v(RILJ_LOG_TAG, "SamsungExynos4RIL: connected, setting network type to " + mPreferredNetworkType);
+                    setPreferredNetworkType(mPreferredNetworkType, null);
+                }
+                break;
             case RIL_UNSOL_STK_PROACTIVE_COMMAND: 
                 Object ret = responseString(p);
                 if (RILJ_LOGD) unsljLogRet(response, ret);
@@ -493,4 +501,16 @@ public class SamsungExynos4RIL extends RIL implements CommandsInterface {
         send(rr);
     }
 
+    @Override
+    public void setPreferredNetworkType(int networkType , Message response) {
+        riljLog("SamsungExynos4RIL: setPreferredNetworkType: " + networkType);
+
+        if (!setPreferredNetworkTypeSeen) {
+            riljLog("SamsungExynos4RIL: Need to reboot modem!");
+            setRadioPower(false, null);
+            setPreferredNetworkTypeSeen = true;
+        }
+
+        super.setPreferredNetworkType(networkType, response);
+    }
 }
