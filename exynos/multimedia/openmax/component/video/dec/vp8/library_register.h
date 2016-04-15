LineageOS/android_hardware_samsung_slsi_exynos5

Update VP8 component role
VP8 component role changed to "video_decoder.vp8" in the framework.

Change-Id: Icba81db7ae921df321e2426ca426b3eaf13fbb98
Signed-off-by: Lajos Molnar <lajos@google.com>
Bug: 9759512

exynos_omx/openmax/exynos_omx/component/video/dec/vp8/library_register.h
@@ -38,7 +38,7 @@
 
 /* VP8 */
 #define EXYNOS_OMX_COMPONENT_VP8_DEC      "OMX.Exynos.VP8.Decoder"
-#define EXYNOS_OMX_COMPONENT_VP8_DEC_ROLE "video_decoder.vpx"
+#define EXYNOS_OMX_COMPONENT_VP8_DEC_ROLE "video_decoder.vp8"
 
 
 #ifdef __cplusplus

Haxynox/android_hardware_samsung

exynos/multimedia/openmax/component/video/dec/vp8/library_register.h
@@ -38,7 +38,7 @@
 
 /* VP8 */
 #define SEC_OMX_COMPONENT_VP8_DEC "OMX.SEC.VP8.Decoder"
-#define SEC_OMX_COMPONENT_VP8_DEC_ROLE "video_decoder.vpx"
+#define SEC_OMX_COMPONENT_VP8_DEC_ROLE "video_decoder.vp8"
 
 
 #ifdef __cplusplus